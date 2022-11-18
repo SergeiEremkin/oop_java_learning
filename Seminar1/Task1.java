f438960dc2f3536f56e981248919f0d1ca6860da
Sergei Eremkin <ffr@list.ru>
2022-11-18 21:47:46 +0300

    Добавил код с первого семинара

--- Seminar1/Task1.java
+++ Seminar1/Task1.java
@@ -1,8 +1,22 @@
 package Seminar1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task1 {
     public static void main(String[] args) {
+        List<Product> prods = new ArrayList<>();
+        prods.add(new Milk("Петмол", 60.0, 5));
+        prods.add(new Product("Чипсы", 60.0));
+        prods.add(new Product("Масло", 50.0));
+        prods.add(new Product("Хлеб", 40.0));
+        prods.add(new Product("Шоколад", 30.0));
+        prods.add(new Product("Снек", 20.0));
+        VendingMachine v1 = new VendingMachine(prods);
+        System.out.println(v1);
+        System.out.println(v1.getProductBy("Масло"));
+        System.out.println(v1.getProductBy(30.0));
         
     }
-    
+
 }
