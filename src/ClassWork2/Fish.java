bad2b8e52cb624d98e9ca057e436f60d47822df4
DwarfPorter <mrvlmor@gmail.com>
2022-11-22 22:02:45 +0300

    2 урок

---
+++ src/ClassWork2/Fish.java
@@ -0,0 +1,9 @@
+package ClassWork2;
+
+public class Fish extends Animal{
+
+    public Fish(String name, String breed, String colour, Integer countLegs, Integer age) {
+        super(name, breed, colour, countLegs, age);
+    }
+
+}
